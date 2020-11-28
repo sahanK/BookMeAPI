c38938021d3f83f9b3a80e67d47311e5a23f6b12
Sahan Kaushalya <sahankaushalya01@gmail.com>
2020-11-28 22:23:07 +0530

    Create Mongoose Schema for Hotel

---
+++ models/Hotel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const HotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description cannot be more than 500 characters'],
+        trim: true
+    },
+    email: {
+        type: String,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'], 
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        province: String,
+        zipcode: String,
+        country: String
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [2, 'Rating cannot be more than 10']
+    },
+    ceatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Hotel', HotelSchema);
