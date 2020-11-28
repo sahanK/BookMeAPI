3dde6af095e7ab909985a0cdc8f16cbfc63780f5
Sahan Kaushalya <sahankaushalya01@gmail.com>
2020-11-28 21:46:50 +0530

    Create a connection with MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db')
+
+// Load env variabls
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Route files
 const hotels = require('./routes/hotels');
 
-// Load env variabls
-dotenv.config({path: './config/config.env'});
+
 
 const app = express();
 
@@ -21,6 +28,13 @@ app.use('/api/v1/hotels', hotels);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`);
-});     
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.blue.bold);
+});   
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exist process
+    server.close(() => process.exit(1));
+})
